f24427f86db9c1df21817e5e264c80048f4ef27d
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-17 15:58:59 -0400

    Beginning styling updates.

--- components/nav.js
+++ components/nav.js
@@ -3,22 +3,51 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <header>
-      <div>
+      <div className="logo">
         <Link href="/">
           <a>Jared A Ritter</a>
         </Link>
       </div>
-      <div>
+      <div className="links">
         <Link href="/projects">
-          <a>Projects</a>
+          <a className="link">Projects</a>
         </Link>
         <Link href="/about">
-          <a>About</a>
+          <a className="link">About</a>
         </Link>
         <Link href="/contact">
-          <a>Contact</a>
+          <a className="link">Contact</a>
         </Link>
       </div>
+      <style jsx>
+        {`
+          header {
+            display: flex;
+            padding: 0.5rem 1.5rem;
+            text-align: center;
+            background-color: rgba(17, 24, 39, 1);
+            color: white;
+            justify-content: space-between;
+          }
+
+          a {
+            color: white;
+          }
+
+          .logo {
+            font-size: 1.875rem;
+            line-height: 2.25rem;
+          }
+
+          .links {
+            padding-top: 0.25rem;
+          }
+
+          .link {
+            margin-right: 0.5rem;
+          }
+        `}
+      </style>
     </header>
   );
 }
