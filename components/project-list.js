d70d5c19a74c67a0d6ea36ad0805d4c5ea4ec7a4
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-17 14:01:34 -0400

    Built project list for getting data.

---
+++ components/project-list.js
@@ -0,0 +1,43 @@
+import utilStyles from '../styles/utils.module.css';
+
+export default function ProjectList({ projects }) {
+  return (
+    <div className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {projects.map(({ id, title, url, comment, use, details, tools }) => (
+        // <li className={utilStyles.listItem} key={id}>
+        //   {title}
+        // </li>
+        <section id={id} key={id}>
+          <h3>
+            <a href={url.deploy}>{title}</a>
+            &nbsp;-&nbsp;
+            <span>
+              <a href={url.source}>Source</a>
+            </span>
+          </h3>
+          {comment ? <p>{comment}</p> : null}
+          <p>
+            <strong>Use:</strong>
+            {use}
+          </p>
+          <p>
+            <strong>Details:</strong>
+            {details}
+          </p>
+          <p>
+            <strong>Build Tools:</strong>
+            {tools.map(({ url, name }) => (
+              <a href={url} key={name}>
+                {name}
+              </a>
+            ))}
+          </p>
+          <p>
+            <a href={url.contribute}>Contribute</a>/
+            <a href={url.issue}>Issue</a>
+          </p>
+        </section>
+      ))}
+    </div>
+  );
+}
