ed8c0a6d9dfd64aa87ea7bba3eebcc28a54306dd
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-20 15:33:39 -0400

    Update footer styling.

--- components/footer.js
+++ components/footer.js
@@ -4,42 +4,59 @@ export default function Footer() {
   return (
     <footer>
       <div id="social">
-        <ul>
-          <li>
-            <a href="https://github.com/jaredaritter">
-              <Image
-                src="/images/GitHub-Mark-32px.png"
-                alt="Small Github Icon"
-                width="28px"
-                height="28px"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/jaredaritter">
-              <Image
-                src="/images/LI-In-Bug-Tiny.png"
-                alt="Small LinkedIn Icon"
-                width="32px"
-                height="32px"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jaredaritter">
-              <Image
-                src="/images/Twitter-Logo-black.svg"
-                alt="Small Twitter Icon"
-                width="32px"
-                height="32px"
-              />
-            </a>
-          </li>
-        </ul>
+        <li>
+          <a href="https://github.com/jaredaritter">
+            <Image
+              src="/images/GitHub-Mark-32px.png"
+              alt="Small Github Icon"
+              width="28px"
+              height="28px"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jaredaritter">
+            <Image
+              src="/images/LI-In-Bug-Tiny.png"
+              alt="Small LinkedIn Icon"
+              width="32px"
+              height="32px"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/jaredaritter">
+            <Image
+              src="/images/Twitter-Logo-black.svg"
+              alt="Small Twitter Icon"
+              width="32px"
+              height="32px"
+            />
+          </a>
+        </li>
       </div>
       <div>
-        <p>&copy; 2020 Jared A Ritter</p>
+        <p id="copyright">&copy; 2020 Jared A Ritter</p>
       </div>
+      <style jsx>
+        {`
+          #social {
+            display: flex;
+            width: 140px;
+            margin: auto;
+          }
+
+          #copyright {
+            text-align: right;
+            margin-right: 10px;
+          }
+
+          li {
+            margin: 0 8px;
+            list-style-type: none;
+          }
+        `}
+      </style>
     </footer>
   );
 }
