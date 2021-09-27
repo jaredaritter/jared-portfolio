251456b0bd02f1510dda35be5b57fb9e75bc014f
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-27 13:58:57 -0400

    Added utility class from module.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import Image from 'next/image';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
@@ -12,10 +13,11 @@ export default function Home() {
         <div className="image-container">
           <Image
             priority
-            src="/images/jared.jpg" // Route to image file
+            src="/images/jared.jpg"
             height={600} // Desired size with correct aspect ratio
             width={400} // Desired size with correct aspect ratio
             alt="Jared headshot"
+            className={utilStyles.borderCircle}
           />
         </div>
         <div id="highlights">
