fe47d1963ed7323e581e75fc67686b76e9345920
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-28 10:56:35 -0400

    Remove unused code.

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,11 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/layout.module.css';
-// import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
 import Header from './header';
 import Footer from './footer';
 
-const name = 'Jared A Ritter';
 export const siteTitle = 'Jared A Ritter';
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -32,43 +28,4 @@ export default function Layout({ children, home }) {
       <Footer />
     </div>
   );
-}
-
-// LEGACY CODE
-{
-  /* <header className={styles.header}>
-  {home ? (
-    <>
-      <Image
-        priority
-        src="/images/jaredAvatar.png"
-        className={utilStyles.borderCircle}
-        height={144}
-        width={144}
-        alt={name}
-      />
-      <h1 className={utilStyles.heading2Xl}>{name}</h1>
-    </>
-  ) : (
-    <>
-      <Link href="/">
-        <a>
-          <Image
-            priority
-            src="/images/jaredAvatar.png"
-            className={utilStyles.borderCircle}
-            height={108}
-            width={108}
-            alt={name}
-          />
-        </a>
-      </Link>
-      <h2 className={utilStyles.headingLg}>
-        <Link href="/">
-          <a className={utilStyles.colorInherit}>{name}</a>
-        </Link>
-      </h2>
-    </>
-  )}
-</header> */
 }
