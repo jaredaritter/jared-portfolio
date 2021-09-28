9c234fd5841973f80431b28752b81183feee96b8
jared <38332939+jaredaritter@users.noreply.github.com>
2021-09-28 14:22:49 -0400

    update meta information on layout.

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/layout.module.css';
 import Header from './header';
 import Footer from './footer';
+import logo from '../public/images/link-logo.png';
 
 export const siteTitle = 'Jared A Ritter';
 
@@ -10,18 +11,44 @@ export default function Layout({ children }) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
+        {/* <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
-        />
-        <meta
+        /> */}
+        {/* <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
+        /> */}
+        {/* <meta name="og:title" content={siteTitle} /> */}
         <meta name="twitter:card" content="summary_large_image" />
+        {/* <!-- HTML Meta Tags --> */}
+        <meta
+          name="description"
+          content="Jared A Ritter's portfolio page. A web developer, Jared is always learning, growing, and contributing. This page contains a list of his projects, a bit of his personality, and ways to contact him."
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://www.jaredaritter.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Jared A Ritter" />
+        <meta
+          property="og:description"
+          content="Jared A Ritter's portfolio page. A web developer, Jared is always learning, growing, and contributing. This page contains a list of his projects, a bit of his personality, and ways to contact him."
+        />
+        <meta property="og:image" content="../public/images/link-logo.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="jaredaritter.com" />
+        <meta property="twitter:url" content="https://www.jaredaritter.com/" />
+        <meta name="twitter:title" content="Jared A Ritter" />
+        <meta
+          name="twitter:description"
+          content="Jared A Ritter's portfolio page. A web developer, Jared is always learning, growing, and contributing. This page contains a list of his projects, a bit of his personality, and ways to contact him."
+        />
+        <meta name="twitter:image" content="../public/images/link-logo.png" />
       </Head>
       <Header />
       <main className={styles.main}>{children}</main>
