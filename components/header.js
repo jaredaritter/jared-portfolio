b3c4c94e71dcb1316e73619a44cde7a52a8619c6
jared <38332939+jaredaritter@users.noreply.github.com>
2021-10-01 10:53:06 -0400

    Finish hover scaling on header links.

--- components/header.js
+++ components/header.js
@@ -1,26 +1,32 @@
 import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Header() {
   return (
     <header>
-      <div className="logo">
+      <div className={`logo ${utilStyles.growSm}`}>
         <Link href="/">
-          <a>Jared A Ritter</a>
+          <a className="grow">Jared A Ritter</a>
         </Link>
       </div>
       <div className="links">
         <Link href="/projects">
-          <a className="link grow">Projects</a>
+          <a className={`link ${utilStyles.growMd}`}>Projects</a>
         </Link>
         <Link href="/about">
-          <a className="link">About</a>
+          <a className={`link ${utilStyles.growMd}`}>About</a>
         </Link>
         <Link href="/contact">
-          <a className="link">Contact</a>
+          <a className={`link ${utilStyles.growMd}`}>Contact</a>
         </Link>
       </div>
       <style jsx>
         {`
+          a {
+            color: white;
+            text-decoration: none;
+          }
+
           header {
             min-width: 280px;
             padding: 0.5rem 1.5rem;
@@ -31,24 +37,6 @@ export default function Header() {
             grid-row-end: 2;
           }
 
-          a {
-            color: white;
-            text-decoration: none;
-            transition: color 0.3s ease-out;
-          }
-
-          a:hover {
-            color: #ddd;
-          }
-
-          .grow {
-            transition: all 0.2s ease-in-out;
-          }
-
-          .grow:hover {
-            transform: scale(1.1);
-          }
-
           .logo {
             font-size: 1.875rem;
             line-height: 2.25rem;
@@ -57,6 +45,7 @@ export default function Header() {
           .links {
             padding-top: 0.25rem;
             display: flex;
+            justify-content: center;
           }
 
           .link {
