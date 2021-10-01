36d14941619aa6d2db828b695d4aa252d7f93101
jared <38332939+jaredaritter@users.noreply.github.com>
2021-10-01 11:08:07 -0400

    Add max-width to header links for improved UX on large displays.

--- components/header.js
+++ components/header.js
@@ -4,21 +4,23 @@ import utilStyles from '../styles/utils.module.css';
 export default function Header() {
   return (
     <header>
-      <div className={`logo ${utilStyles.growSm}`}>
-        <Link href="/">
-          <a className="grow">Jared A Ritter</a>
-        </Link>
-      </div>
-      <div className="links">
-        <Link href="/projects">
-          <a className={`link ${utilStyles.growMd}`}>Projects</a>
-        </Link>
-        <Link href="/about">
-          <a className={`link ${utilStyles.growMd}`}>About</a>
-        </Link>
-        <Link href="/contact">
-          <a className={`link ${utilStyles.growMd}`}>Contact</a>
-        </Link>
+      <div className="container">
+        <div className={`logo ${utilStyles.growSm}`}>
+          <Link href="/">
+            <a className="grow">Jared A Ritter</a>
+          </Link>
+        </div>
+        <div className="links">
+          <Link href="/projects">
+            <a className={`link ${utilStyles.growMd}`}>Projects</a>
+          </Link>
+          <Link href="/about">
+            <a className={`link ${utilStyles.growMd}`}>About</a>
+          </Link>
+          <Link href="/contact">
+            <a className={`link ${utilStyles.growMd}`}>Contact</a>
+          </Link>
+        </div>
       </div>
       <style jsx>
         {`
@@ -29,12 +31,17 @@ export default function Header() {
 
           header {
             min-width: 280px;
-            padding: 0.5rem 1.5rem;
-            text-align: center;
             background-color: rgba(17, 24, 39, 1);
-            justify-content: space-between;
             grid-row-start: 1;
             grid-row-end: 2;
+          }
+
+          .container {
+            max-width: 1000px;
+            margin: auto;
+            padding: 0.5rem 1.5rem;
+            text-align: center;
+            justify-content: space-between;
           }
 
           .logo {
@@ -54,7 +61,7 @@ export default function Header() {
           }
 
           @media screen and (min-width: 550px) {
-            header {
+            .container {
               display: flex;
             }
           }
