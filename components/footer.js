72be05446c1c0431b831505ee0d611a82172f02e
jared <38332939+jaredaritter@users.noreply.github.com>
2021-10-19 16:34:36 -0400

    Add Issue link to footer so visitors can let me know about page issues.

--- components/footer.js
+++ components/footer.js
@@ -1,43 +1,51 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div id={styles.social}>
-        <li className={styles.li}>
-          <a href="https://github.com/jaredaritter">
-            <Image
-              src="/images/GitHub-Mark-32px.png"
-              alt="Small Github Icon"
-              width="28px"
-              height="28px"
-            />
-          </a>
-        </li>
-        <li className={styles.li}>
-          <a href="https://www.linkedin.com/in/jaredaritter">
-            <Image
-              src="/images/LI-In-Bug-Tiny.png"
-              alt="Small LinkedIn Icon"
-              width="32px"
-              height="32px"
-            />
-          </a>
-        </li>
-        <li className={styles.li}>
-          <a href="https://twitter.com/jaredaritter">
-            <Image
-              src="/images/Twitter-Logo-black.svg"
-              alt="Small Twitter Icon"
-              width="32px"
-              height="32px"
-            />
-          </a>
-        </li>
-      </div>
-      <div>
-        <p id={styles.copyright}>&copy; 2020 Jared A Ritter</p>
+      <div className={styles.container}>
+        <div className={styles.upper}>
+          <li className={styles.li}>
+            <a href="https://github.com/jaredaritter">
+              <Image
+                src="/images/GitHub-Mark-32px.png"
+                alt="Small Github Icon"
+                width="28px"
+                height="28px"
+              />
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="https://www.linkedin.com/in/jaredaritter">
+              <Image
+                src="/images/LI-In-Bug-Tiny.png"
+                alt="Small LinkedIn Icon"
+                width="32px"
+                height="32px"
+              />
+            </a>
+          </li>
+          <li className={styles.li}>
+            <a href="https://twitter.com/jaredaritter">
+              <Image
+                src="/images/Twitter-Logo-black.svg"
+                alt="Small Twitter Icon"
+                width="32px"
+                height="32px"
+              />
+            </a>
+          </li>
+        </div>
+        <div className={styles.lower}>
+          <Link href="https://github.com/jaredaritter/jared-portfolio/issues">
+            <a className={`${styles.p} ${styles.a}`} target="_blank">
+              Issues
+            </a>
+          </Link>
+          <p className={styles.p}>&copy; 2020 Jared A Ritter</p>
+        </div>
       </div>
     </footer>
   );
