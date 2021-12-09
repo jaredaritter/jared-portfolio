6d072c10b7d8446bb434acde664fcfa2b70cfdbf
jared <38332939+jaredaritter@users.noreply.github.com>
2021-12-09 14:30:44 -0500

    Update about page content , styling, and semantic markup.

--- pages/about.js
+++ pages/about.js
@@ -12,43 +12,38 @@ export default function About() {
       <section className={styles.section}>
         <h2 className={styles.h2}>$ whoami</h2>
         <div className={styles.details}>
-          <p>
-            <strong>Professional</strong>
-          </p>
-          <p>
+          <h3 className={styles.h3}>Professional</h3>
+          <p className={styles.p}>
             I am a web developer, and I love to learn from those around me. I
             come from a healthcare and academic background and have a passion
             for knowledge, teamwork, and service.
           </p>
-          <p>
-            I am a problem solver with a strong interest in performance,
-            efficiency, minimalism, and clean design. I believe that a good user
-            experience is more than what is seen on the page. I love making
-            things work, and then making them work better.
+          <p className={styles.p}>
+            I have a strong interest in performance, efficiency, minimalism, and
+            clean design. I believe that a good user experience is more than
+            what is seen on the page. I love making things work, and then making
+            them work better.
           </p>
-          <p>
-            <strong>Personal</strong>
-          </p>
-          <p>
+          <h3 className={styles.h3}>Personal</h3>
+          <p className={styles.p}>
             I am a transplant to the Research Triangle area and find myself
-            enjoying it immensely. Coffee shops, breweries, and local eats would
-            be the norm, but I have been more conservative due to the pandemic.
-            I'm a soccer player, climber, and I always enjoy getting out for a
-            hike with my four-legged creature.
+            enjoying it immensely. It strikes a nice balance between having city
+            life and having access to the outdoors, and the coffee shops,
+            breweries, and restaurants continue to please. I'm a soccer player,
+            book-lover, and I enjoy getting out for a hike with my four-legged
+            creature.
           </p>
-          <p>
-            <strong>History</strong>
-          </p>
-          <p>
+          <h3 className={styles.h3}>History (the longish short version)</h3>
+          <p className={styles.p}>
             I'm a lifelong tech lover who was pulled in a different direction
             for the first decade of my professional life. Working in physical
             therapy provided me with a fulfilling way to help people, make a
             difference, and have amazing experiences. I was also fortunate to
             meet wonderful patients and co-workers along the way and can say
-            that I am a better person for it. Ultimately, it was missing
-            something though. Growth and challenge.
+            that I am a better person for it. Ultimately, I was missing
+            something. Challenge and change.
           </p>
-          <p>
+          <p className={styles.p}>
             Outside of treating professional athletes and performing research
             studies, physical therapy is a relatively stable field. There are
             long delays between research results and their application in the
@@ -60,19 +55,18 @@ export default function About() {
             downside is that truly novel learning experiences are limited and
             problem solving is more human-oriented than treatment-oriented.
           </p>
-          <p>
+          <p className={styles.p}>
             The longer I worked, the more I came to realize that what I desired
             in a career was continuous learning, problem solving, collaboration,
             and creation. I wanted to learn and contribute that knowledge to
-            something bigger. Prior to physical therapy, I spent time in the
-            computer science program and found it challenging, amazing, and
-            terrifying, and I was ultimately pulled away to healthcare. It was
-            the right choice at that point in my life for many reasons, but as I
-            have matured I have come to realize that the challenge and curiosity
-            that was sparked in those classes is still there and is where I want
-            to devote my energy.
+            something bigger. Prior to physical therapy, I was in the computer
+            science program and found it challenging, amazing, terrifying, and
+            ultimately I was pulled away to healthcare. It was the right choice
+            at that point in my life, but as I matured I came to realize that
+            the challenge and curiosity that was sparked in those classes was
+            still there.
           </p>
-          <p>
+          <p className={styles.p}>
             And so I come full circle, bringing my career back to software and
             web development. It is a career I that I find more interesting and
             fulfilling each day as I wander further down the rabbit hole. There
